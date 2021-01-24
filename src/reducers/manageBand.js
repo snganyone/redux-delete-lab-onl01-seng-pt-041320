ada7565e652259da076cab159ff278a03c1c0c73
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2021-01-24 15:54:16 -0800

    Done.

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,10 +1,16 @@
+import uuid from 'uuid';
+
 export default function manageBand(state = {
   bands: []
 }, action) {
   switch (action.type) {
     case 'ADD_BAND':
 
-      return { ...state, bands: [...state.bands, action.name] }
+      const band = {
+        id: uuid(),
+        name: action.name
+      }
+      return { ...state, bands: [...state.bands, band] }
 
     default:
       return state;
